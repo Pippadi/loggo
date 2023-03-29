8f94d6cd3808d400d9e6062f7e8265cdfb13273e
Prithvi Vishak <prithvivishak@gmail.com>
2023-03-29 14:00:40 +0530

    Comment code

--- loggo.go
+++ loggo.go
@@ -6,15 +6,17 @@ import (
 	"time"
 )
 
-// Requirements:
-// - Minimal interruption to calling function
-// - Redirection to file (optional)
-
 type Level int
 
 const (
+	// DebugLevel is the lowest log level
 	DebugLevel Level = iota
+
+	// InfoLevel is the default log level, and is higher than
+	// DebugLevel but lower than ErrorLevel
 	InfoLevel
+
+	// ErrorLevel is the highest log level
 	ErrorLevel
 )
 
@@ -33,34 +35,54 @@ var prefixes = [3]string{
 var level Level = InfoLevel
 var outfile = os.Stdout
 
+// SetLevel sets log level to one of DebugLevel, InfoLevel, or ErrorLevel.
+// Log priority is given by DebugLevel < InfoLevel < ErrorLevel.
+// Log level is InfoLevel by default.
 func SetLevel(lvl Level) {
 	level = lvl
 }
+
+// SetFile sets a log file. It is the calling function's responsibility to
+// open and close the file. Default output is os.Stdout.
 func SetFile(f *os.File) {
 	outfile = f
 }
 
-func Debug(msgs ...any) {
-	printRaw(DebugLevel, msgs...)
+// Debug prints arguments followed by a newline at DebugLevel.
+func Debug(args ...any) {
+	printRaw(DebugLevel, args...)
 }
+
+// Debugf prints arguments formatted by the given template at DebugLevel.
+// A newline character is always appended to the final string.
 func Debugf(template string, args ...any) {
 	Debug(fmt.Sprintf(template, args...))
 }
 
-func Info(msgs ...any) {
-	printRaw(InfoLevel, msgs...)
+// Info prints arguments followed by a newline at InfoLevel.
+func Info(args ...any) {
+	printRaw(InfoLevel, args...)
 }
+
+// Infof prints arguments formatted by the given template at InfoLevel.
+// A newline character is always appended to the final string.
 func Infof(template string, args ...any) {
 	Info(fmt.Sprintf(template, args...))
 }
 
-func Error(msgs ...any) {
-	printRaw(ErrorLevel, msgs...)
+// Error prints arguments followed by a newline at ErrorLevel.
+func Error(args ...any) {
+	printRaw(ErrorLevel, args...)
 }
+
+// Errorf prints arguments formatted by the given template at ErrorLevel.
+// A newline character is always appended to the final string.
 func Errorf(template string, args ...any) {
 	Error(fmt.Sprintf(template, args...))
 }
 
+// printRaw conditionally prints args with the current
+// timestamp and colorized log level prefix
 func printRaw(lvl Level, args ...any) {
 	if lvl >= level {
 		outfile.WriteString(fmt.Sprintln(append(
